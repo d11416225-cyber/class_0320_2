b78f155147125b6bb980dd207e5150b64503b943
d11416225-cyber <d11416225@gs.takming.edu.tw>
2026-03-20 11:31:54 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace class_0320_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test std1, std2;
+            std1 = new Test();
+            std2 = new Test();
+
+            std1.Mid = 78;
+            std1.SetFinal(72);
+
+            std2.SetGrade(-68, 100);
+
+
+            Console.WriteLine("第一位" + std1.GetAvg().ToString());
+
+            Console.WriteLine("第二位" + std1.GetAvg().ToString());
+        }
+    }
+}
